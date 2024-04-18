ea0e9a7d05a07c149acbf6edc3dcd17014e802cd
Shitikov Kirill <k.shytikov@redgenn.com>
2024-04-18 15:35:24 +0200

    get git branch

--- git_naming_hooks/check_git_branch.py
+++ git_naming_hooks/check_git_branch.py
@@ -1,4 +1,17 @@
-import sys
+import argparse
+import os
+from typing import Any
+from typing import Sequence
 
-def main():
-    print(sys.argv[:])
+
+def main(argv: Sequence[str] | None = None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('regexps', nargs='*', help='Filenames to check.')
+    args = parser.parse_args(argv)
+    branch = os.popen("git rev-parse --abbrev-ref HEAD").read()
+    print(branch)
+    print(args)
+    return 1
+    
+if __name__ == '__main__':
+    raise SystemExit(main())
