5dd54f0f6e259a6ab9b2f2dda34f98656a1ad0de
Shitikov Kirill <k.shytikov@redgenn.com>
2024-04-18 15:40:24 +0200

    Get args

--- git_naming_hooks/check_git_branch.py
+++ git_naming_hooks/check_git_branch.py
@@ -1,16 +1,18 @@
 import argparse
 import os
+import sys
 from typing import Any
 from typing import Sequence
 
 
 def main(argv: Sequence[str] | None = None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('regexps', nargs='*', help='Filenames to check.')
+    parser.add_argument('filenames', nargs='*', help='Filenames to check.')
     args = parser.parse_args(argv)
     branch = os.popen("git rev-parse --abbrev-ref HEAD").read()
     print(branch)
     print(args)
+    print(sys.argv[:])
     return 1
     
 if __name__ == '__main__':
