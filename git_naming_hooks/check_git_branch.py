7511c67575ec39ea239c34b22219478ff2bf14c6
Shitikov Kirill <k.shytikov@redgenn.com>
2024-04-18 15:50:48 +0200

    Update regexp

--- git_naming_hooks/check_git_branch.py
+++ git_naming_hooks/check_git_branch.py
@@ -1,14 +1,21 @@
 import argparse
 import os
 import sys
-from typing import Any
 from typing import Sequence
 
 
 def main(argv: Sequence[str] | None = None):
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to check.')
+    parser.add_argument(
+        '-r', '--regexp',
+        type=list, action='store',
+        dest='list',
+        help='List of branch regexp',
+        required=True
+    )
     args = parser.parse_args(argv)
+
     branch = os.popen("git rev-parse --abbrev-ref HEAD").read()
     print(branch)
     print(args)
