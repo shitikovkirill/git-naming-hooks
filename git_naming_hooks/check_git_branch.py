650021122a3728edf869762633fedffde65f4cd7
Shitikov Kirill <k.shytikov@redgenn.com>
2024-04-18 15:55:21 +0200

    Fix

--- git_naming_hooks/check_git_branch.py
+++ git_naming_hooks/check_git_branch.py
@@ -9,9 +9,7 @@ def main(argv: Sequence[str] | None = None):
     parser.add_argument('filenames', nargs='*', help='Filenames to check.')
     parser.add_argument(
         '-r', '--regexp',
-        type=list, action='store',
-        dest='list',
-        help='List of branch regexp',
+        help='Branch regexp',
         required=True
     )
     args = parser.parse_args(argv)
