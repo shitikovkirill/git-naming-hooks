4f922f7be6e0e0800f3cf57826d128a9eaaddc04
Shitikov Kirill <shitikovkirillt@gmail.com>
2024-04-19 11:32:48 +0200

    Fix dubl check

--- git_naming_hooks/check_git_branch.py
+++ git_naming_hooks/check_git_branch.py
@@ -6,26 +6,19 @@ from typing import Sequence
 
 def main(argv: Sequence[str] | None = None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*', help='Filenames to check.')
-    parser.add_argument(
-        '-r', '--regexp',
-        help='Branch regexp',
-        required=True
-    )
+    parser.add_argument("filenames", nargs="*", help="Filenames to check.")
+    parser.add_argument("-r", "--regexp", help="Branch regexp", required=True)
     args = parser.parse_args(argv)
 
     pattern = re.compile(args.regexp)
     branch = os.popen("git rev-parse --abbrev-ref HEAD").read()
-    
+
     print(f"We match pattern {pattern} with branch '{branch}'.")
 
     match = re.search(pattern, branch)
     if match:
         print("Branch is correct.")
         return 0
-    
+
     print("Branch has not correct format.")
     return 1
-    
-if __name__ == '__main__':
-    raise SystemExit(main())
--- git_naming_hooks/check_git_commit.py
+++ git_naming_hooks/check_git_commit.py
@@ -1,36 +1,28 @@
 import argparse
-import os
 import re
 from typing import Sequence
 
 
 def main(argv: Sequence[str] | None = None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*', help='Filenames to check.')
-    parser.add_argument(
-        '-r', '--regexp',
-        help='Branch regexp',
-        required=True
-    )
-    parser.add_argument('message_file') 
+    parser.add_argument("filenames", nargs="*", help="Filenames to check.")
+    parser.add_argument("-r", "--regexp", help="Branch regexp", required=True)
+    parser.add_argument("message_file")
     args = parser.parse_args(argv)
 
     pattern = re.compile(args.regexp)
-    
+
     with open(args.message_file) as f:
         contents = f.readlines()
 
     message = " ".join(contents)
-    
-    print(f"We match pattern {pattern} with message commit '{message[:10]}...'.")
+
+    print(f"We match pattern {pattern} with message commit '{message[:9]}...'")
 
     match = re.search(pattern, message)
     if match:
         print("Commit message is correct.")
         return 0
-    
+
     print("Commit message has not correct format.")
     return 1
-    
-if __name__ == '__main__':
-    raise SystemExit(main())
